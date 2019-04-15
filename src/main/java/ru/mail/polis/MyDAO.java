94d16323900f0a26950d6a62b5657e803dd33945
blaackpanter <pranowa.l@yandex.ru>
2019-04-15 15:20:16 +0300

    final

--- src/main/java/ru/mail/polis/MyDAO.java
+++ src/main/java/ru/mail/polis/MyDAO.java
@@ -13,18 +13,18 @@ public class MyDAO implements DAO {
 
     @NotNull
     @Override
-    public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
         return base.tailMap(from).values().iterator();
     }
 
 
     @Override
-    public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) throws IOException {
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) throws IOException {
             base.put(key, Record.of(key, value));
     }
 
     @Override
-    public void remove(@NotNull ByteBuffer key) throws IOException {
+    public void remove(@NotNull final ByteBuffer key) throws IOException {
             base.remove(key);
     }
 
